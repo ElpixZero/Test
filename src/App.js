5015136401fb68d28b39990fe3a3d6560f44af2d
ElpixZero <elnasurov@gmail.com>
2020-02-08 17:11:30 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import Table2 from './components/Tablee';
+import Table from './components/Table';
 import InitialScreen from './components/InitialScreen';
 
 import './App.css';
@@ -15,13 +15,12 @@ const THEME = createMuiTheme({
 function App() {
   const [source, setSource] = React.useState('');
 
-  console.log('app updating');
-
   return (
     <MuiThemeProvider theme={THEME}>
-      <div style={{paddingTop: 50, minHeight: '100vh', margin: '0 auto', color: 'yellow', backgroundColor: 'black'}}>
-        {!source &&  <InitialScreen setSource={setSource} />}
-        {source && <Table2 style={{marginBottom: 100 }} source={source} />}
+      <div style={{paddingTop: 20, minHeight: '100vh', margin: '0 auto', color: 'yellow', backgroundColor: 'black'}}>
+        {source ? <Table source={source} />
+          : <InitialScreen setSource={setSource} />
+        }
       </div>
     </MuiThemeProvider>
   );
