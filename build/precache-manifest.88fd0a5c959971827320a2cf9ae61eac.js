ea461f57f8dca8df7283f805da8965fd42c59a68
ElpixZero <elnasurov@gmail.com>
2020-02-08 23:16:36 +0300

    Build

---
+++ build/precache-manifest.88fd0a5c959971827320a2cf9ae61eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effed3dd01d8d38dc7fa2e7ac59ba0e2",
+    "url": "/ArchakovTest/index.html"
+  },
+  {
+    "revision": "29a923550e8fc51a31ea",
+    "url": "/ArchakovTest/static/css/main.8ff5a9f6.chunk.css"
+  },
+  {
+    "revision": "6c4d4be02dabab9de118",
+    "url": "/ArchakovTest/static/js/2.48a62985.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/ArchakovTest/static/js/2.48a62985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a923550e8fc51a31ea",
+    "url": "/ArchakovTest/static/js/main.3bc04f26.chunk.js"
+  },
+  {
+    "revision": "30d3eae8d7bb43c0bbbd",
+    "url": "/ArchakovTest/static/js/runtime-main.0f7c8377.js"
+  }
+]);
