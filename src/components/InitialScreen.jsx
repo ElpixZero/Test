ac718390256e1bf1597ec19f78123ec6e3ea245b
ElpixZero <elnasurov@gmail.com>
2020-02-09 15:49:23 +0300

    Improved url path to data

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -4,8 +4,8 @@ import {CssBaseline, Container, Button, ButtonGroup} from '@material-ui/core';
 
 function SimpleContainer({setSource}) {
   const ulrs = {
-    small: 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
-    big: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+    small: 'https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
+    big: 'https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
   };
 
   return (
