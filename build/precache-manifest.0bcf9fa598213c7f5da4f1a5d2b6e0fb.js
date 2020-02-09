b381ed50ffa58e625affa02eed232f5499b4221b
ElpixZero <elnasurov@gmail.com>
2020-02-09 15:49:36 +0300

    build

---
+++ build/precache-manifest.0bcf9fa598213c7f5da4f1a5d2b6e0fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31526fde2d8ee98e260a23af00c0cbb",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "29a923550e8fc51a31ea",
+    "url": "/Test/static/css/main.8ff5a9f6.chunk.css"
+  },
+  {
+    "revision": "6c4d4be02dabab9de118",
+    "url": "/Test/static/js/2.48a62985.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Test/static/js/2.48a62985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a923550e8fc51a31ea",
+    "url": "/Test/static/js/main.3bc04f26.chunk.js"
+  },
+  {
+    "revision": "f3faf9e0504216869896",
+    "url": "/Test/static/js/runtime-main.b1df66fe.js"
+  }
+]);
